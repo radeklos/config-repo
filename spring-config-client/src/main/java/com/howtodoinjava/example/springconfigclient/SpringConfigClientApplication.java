ccfa64dea7f69fead4b4d0b6d9b78211d4aa980a
radek.los <radek.los@ataccama.com>
2020-01-10 11:29:09 +0100

    Config service to my github

--- spring-config-client/src/main/java/com/howtodoinjava/example/springconfigclient/SpringConfigClientApplication.java
+++ spring-config-client/src/main/java/com/howtodoinjava/example/springconfigclient/SpringConfigClientApplication.java
@@ -1,11 +1,11 @@
 package com.howtodoinjava.example.springconfigclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,23 +15,72 @@ public class SpringConfigClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConfigClientApplication.class, args);
 	}
-	
-	@Autowired
-	public void setEnv(Environment e)
-	{
-		System.out.println(e.getProperty("msg"));
+}
+
+
+@Configuration
+class Config {
+
+	@Bean
+	@RefreshScope
+	public Data data(@Value("${foo:Config Server is not working. Please check...}") String msg) {
+		return new Data(msg);
 	}
+
+	@Bean
+	public String msg(@Value("${foo:Config Server is not working. Please check...}") String msg) {
+		return msg;
+	}
+
 }
 
 @RefreshScope
 @RestController
 class MessageRestController {
 
-	@Value("${msg:Config Server is not working. Please check...}")
-	private String msg;
+	private String msgValue;
+	private String msgBean;
+	private Data data;
+
+	public MessageRestController(Data data, String msgBean, @Value("${foo:Config Server is not working. Please check...}") String msgValue) {
+		this.data = data;
+		this.msgValue = msgValue;
+		this.msgBean = msgBean;
+	}
 
 	@GetMapping("/msg")
+	public Response getMsg() {
+		return new Response(null, msgValue, msgBean);
+	}
+
+	@GetMapping("/data")
+	public String getData() {
+		return data.getMsg();
+	}
+}
+
+class Data {
+
+	private final String msg;
+
+	public Data(String msg) {
+		this.msg = msg;
+	}
+
 	public String getMsg() {
-		return this.msg;
+		return msg;
+	}
+
+}
+
+class Response {
+	public final Data data;
+	public final String msgValue;
+	public final String msgBean;
+
+	public Response(Data data, String msgValue, String msgBean) {
+		this.data = data;
+		this.msgValue = msgValue;
+		this.msgBean = msgBean;
 	}
 }
